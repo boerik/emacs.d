6bed66f8f4e4c9506b8f97e62cf4728863c6d8e8
Erik Stenberg <boe.stenberg@gmail.com>
2015-10-18 15:51:35 +0200

    minor

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,7 @@ git pull origin master;
 
 function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
-		--exclude "README.md" --exclude "LICENSE" -avh --no-perms . ~/emacs.d/;
+		--exclude "README.md" --exclude "LICENSE" -avh --no-perms . ~/.emacs.d;
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
